5184a117e0e9933442c50bac523eb125cde82046
Carolina Papes <papes.caro@gmail.com>
2026-02-23 14:54:49 +0100

    [37]-backend-minimun-setup

---
+++ src/backend/app/test.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+
+int	main(void)
+{
+	write(1, "1114 Mundo\n", 11);
+}
---
+++ src/backend/docker/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+echo "🚀 Starting Backend Container..."
+
+
+if [ ! -d "node_modules" ] || [ -z "$(ls -A node_modules 2>/dev/null)" ]; then
+  echo "📦 node_modules not found. Installing dependencies..."
+
+  if [ ! -f "package-lock.json" ]; then
+    echo "📝 No package-lock.json. Running npm install (bootstrap)..."
+    npm install
+  else
+    echo "🔒 Using lockfile. Running npm ci..."
+    npm ci
+  fi
+
+else
+  echo "✅ Dependencies already installed."
+fi
+
+echo "▶️ Starting Express.js..."
+exec "$@"
