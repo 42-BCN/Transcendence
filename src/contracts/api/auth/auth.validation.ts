fdb9c3011e699355beecd54fcb8988d425ce3404
Ziermax <maxi007velazquez@gmail.com>
2026-03-04 18:10:46 +0100

    email Schemas redefined, no more trimReq

--- src/contracts/api/auth/auth.validation.ts
+++ src/contracts/api/auth/auth.validation.ts
@@ -3,24 +3,22 @@ import { z } from "zod";
 import { VALIDATION as V } from "../http/validation";
 import { safeParseSchema } from "../lib";
 
-export const trimRequiredString = z
+// export const trimRequiredString = z
+//   .string()
+//   .trim()
+//   .min(1, { message: V.REQUIRED });
+
+export const emailSchema = z.email({ message: V.INVALID_EMAIL });
+// TODO .transform((val) => val.toLowerCase().trim());
+
+export const usernameSchema = z
   .string()
   .trim()
-  .min(1, { message: V.REQUIRED });
-
-export const emailSchema = z
-  .string()
-  .trim()
-  .min(1, { message: V.REQUIRED })
-  .max(254, { message: V.FIELD_TOO_LONG })
-  .pipe(z.email({ message: V.INVALID_EMAIL }));
-
-export const usernameSchema = trimRequiredString
   .min(3, { message: V.FIELD_TOO_SHORT })
   .max(30, { message: V.FIELD_TOO_LONG })
   .regex(/^[\w-]+$/, { message: V.INVALID_USERNAME });
 
-export const identifierSchema = trimRequiredString.superRefine((val, ctx) => {
+export const identifierSchema = z.string().superRefine((val, ctx) => {
   if (val.includes("@")) {
     safeParseSchema(emailSchema, val, ctx);
   } else {
