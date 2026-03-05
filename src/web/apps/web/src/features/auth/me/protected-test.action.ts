d61d2c1185c9add7c66638f57562721983950402
Carolina Papes <papes.caro@gmail.com>
2026-03-05 19:16:37 +0100

    auth [#60] update me route

--- src/web/apps/web/src/features/auth/me/protected-test.action.ts
+++ src/web/apps/web/src/features/auth/me/protected-test.action.ts
@@ -1,16 +1,12 @@
 'use server';
 
+import type { MeRes } from '@/contracts/auth/auth.contract';
 import { cookies } from 'next/headers';
 import { fetchServer } from '@/lib/http/fetcher.server';
 
 export async function protectedMeAction() {
   const cookie = (await cookies()).toString();
-  console.log(cookie);
-  try {
-    const body = await fetchServer('/protected/me', 'GET', undefined, { cookie });
-    console.log(body);
-    return { ok: true, body };
-  } catch (e: any) {
-    return { ok: false, error: e?.message ?? 'Unknown error' };
-  }
+
+  const { data } = await fetchServer<MeRes>('/protected/me', 'GET', undefined, { cookie });
+  return data;
 }
