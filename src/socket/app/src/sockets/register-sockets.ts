fcf4f91dba03b8d4d4e29f836f21c71410ef3b0c
Carolina Papes <papes.caro@gmail.com>
2026-03-16 20:05:01 +0100

    SOCKET [#76] Change register name to add more files with same pattern name

--- src/socket/app/src/sockets/register-sockets.ts
+++
@@ -1,6 +0,0 @@
-import type { Server } from 'socket.io';
-import { registerRobotsSocket } from '../features/robots.socket';
-
-export function registerSockets(io: Server) {
-  registerRobotsSocket(io.of('/robots'));
-}
