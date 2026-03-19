3c0f79aa980ebb5c4624bf5975ba351b435cf9b4
Carolina Papes <papes.caro@gmail.com>
2026-03-19 17:08:52 +0100

    UI [#139] index WIP

---
+++ containers/frontend/web/src/components/index.ts
@@ -0,0 +1,17 @@
+// composites
+export { Drawer } from './composites/drawer/drawer';
+export { Form, type FormProps } from './composites/form/form';
+export {
+  SegmentedControlGroup,
+  type SegmentedControlGroupProps,
+} from './composites/segmented-control-group/segmented-control-group';
+export {
+  TextAreaField,
+  type TextAreaFieldProps,
+} from './composites/text-area-field/text-area-field';
+export { TextField, type TextFieldProps } from './composites/text-field/text-field';
+export { TooltipTrigger } from './composites/tooltip-trigger/tooltip-trigger';
+
+// controls
+
+// primitives
