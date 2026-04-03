028b9858ebf525d17e540a8346e518f567370c5f
Cristian Angelo <crismac@MacBook-Air-de-Cristian.local>
2026-04-03 16:38:30 +0200

    added game sockets to the socket register

--- containers/socket/app/src/sockets/socket.register.ts
+++ containers/socket/app/src/sockets/socket.register.ts
@@ -1,9 +1,11 @@
 import type { Server } from 'socket.io';
 
 import { registerRobotsSocket } from '../features/robots.socket';
+import { registerGameSocket } from '../features/robots.socket';
 import { registerChatSocket } from '../features/chat.socket';
 
 export function registerSockets(io: Server) {
   registerRobotsSocket(io.of('/robots'));
+  registerGameSocket(io.of('/game'));
   registerChatSocket(io.of('/chat'));
 }
