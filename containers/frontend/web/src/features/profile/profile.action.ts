8fd56dcf3db7e583620d34b6745ae8f7c7111a66
Marta Fontseré <martagesplu@yahoo.es>
2026-04-27 01:36:47 +0200

    wrap protectedMeProfileAction with withServerAction helper

--- containers/frontend/web/src/features/profile/profile.action.ts
+++ containers/frontend/web/src/features/profile/profile.action.ts
@@ -3,18 +3,22 @@
 import { type UserMeProfileResponse } from '@/contracts/api/users/users.contracts';
 
 import { cookies } from 'next/headers';
-import { fetchServer } from '@/lib/http/fetcher.server';
+import { fetchServer, withServerAction } from '@/lib/http/fetcher.server';
 
 export async function protectedMeProfileAction() {
   const cookie = (await cookies()).toString();
 
-  const { data } = await fetchServer<UserMeProfileResponse>(
-    '/protected/me/profile',
-    'GET',
-    undefined,
-    {
-      cookie,
-    },
-  );
-  return data;
+  const result = await withServerAction(async () => {
+    const response = await fetchServer<UserMeProfileResponse>(
+      '/protected/me/profile',
+      'GET',
+      undefined,
+      {
+        cookie,
+      },
+    );
+    return response.data;
+  })();
+
+  return result;
 }
