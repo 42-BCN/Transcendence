32e8a37c93bc716970e23158ef6577ce8a383d13
carolinapapes <64616132+carolinapapes@users.noreply.github.com>
2026-06-18 04:54:01 +0200

    Start up fixes

--- containers/backend/docker/docker-entrypoint.sh
+++ containers/backend/docker/docker-entrypoint.sh
@@ -16,9 +16,6 @@ if [ "$NODE_ENV" = "development" ]; then
   else
     echo "Dependencies already installed."
   fi
-
-  echo "Generating Prisma client..."
-  npm run prisma:generate
 fi
 
 echo "Starting Express.js..."
